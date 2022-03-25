ff9811ca813138fc3d2f1bc14454d568783b92fe
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-03-25 07:50:22 -0500

    Cliente controller done.

--- src/main/java/info/microdev/payroll/controller/ClienteController.java
+++ src/main/java/info/microdev/payroll/controller/ClienteController.java
@@ -1,10 +1,55 @@
 package info.microdev.payroll.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import info.microdev.payroll.model.Cliente;
+import info.microdev.payroll.repository.ClienteRepository;
+import org.springframework.expression.ExpressionException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClienteController {
 
+    private final ClienteRepository clienteRepository;
 
+    public ClienteController(ClienteRepository clienteRepository){
+        this.clienteRepository = clienteRepository;
+    }
+
+    @GetMapping("/clientes")
+    public List<Cliente> all(){
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping("/clientes")
+    public Cliente newCliente(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+
+    //TODO: Add a throwable exception
+    @GetMapping("/clientes/{id}")
+    public Cliente one(@PathVariable String id){
+        return clienteRepository.findById(id).orElseThrow(() -> new ExpressionException("Error!"));
+    }
+
+    @PutMapping("/clientes/{id}")
+    public Cliente replaceEmployee(@RequestBody Cliente newCliente, @PathVariable String id){
+        return clienteRepository.findById(id)
+                .map(cliente ->{
+                    cliente.setDireccion(newCliente.getDireccion());
+                    cliente.setEmail(newCliente.getEmail());
+                    cliente.setNombre(newCliente.getNombre());
+                    cliente.setTelefono(newCliente.getTelefono());
+                    return clienteRepository.save(cliente);
+                }).orElseGet(()->{
+                    newCliente.setCedula(id);
+                    return clienteRepository.save(newCliente);
+                });
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    public void deleteCliente(@PathVariable String id){
+        clienteRepository.deleteById(id);
+    }
 
 }
