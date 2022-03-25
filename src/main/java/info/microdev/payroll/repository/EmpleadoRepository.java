b8ef58708cf590aa0a2283a3294a62b3954b7dc8
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-03-25 14:00:51 -0500

    Named query for getting an employee based on user and password.

--- src/main/java/info/microdev/payroll/repository/EmpleadoRepository.java
+++ src/main/java/info/microdev/payroll/repository/EmpleadoRepository.java
@@ -2,6 +2,13 @@ package info.microdev.payroll.repository;
 
 import info.microdev.payroll.model.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface EmpleadoRepository extends JpaRepository<Empleado, String> {
+
+    @Query(value = "SELECT * FROM empleado WHERE cedula = ?1 and password = ?2", nativeQuery = true)
+    Optional<Empleado> findByUserAndPassword(String cedula, String password);
+
 }
