6c6dcd9b6654c63a37f507a697fee634aaded3a5
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-03-25 14:01:06 -0500

    Updates in repositories.

--- src/main/java/info/microdev/payroll/repository/FacturaDetalleRepository.java
+++ src/main/java/info/microdev/payroll/repository/FacturaDetalleRepository.java
@@ -1,6 +1,7 @@
 package info.microdev.payroll.repository;
 
+import info.microdev.payroll.model.FacturaDetalle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FacturaDetalleRepository extends JpaRepository<FacturaDetalleRepository, Long> {
+public interface FacturaDetalleRepository extends JpaRepository<FacturaDetalle, Long> {
 }
--- src/main/java/info/microdev/payroll/repository/ProveedorRepository.java
+++ src/main/java/info/microdev/payroll/repository/ProveedorRepository.java
@@ -1,6 +1,7 @@
 package info.microdev.payroll.repository;
 
+import info.microdev.payroll.model.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProveedorRepository extends JpaRepository<ProveedorRepository, String> {
+public interface ProveedorRepository extends JpaRepository<Proveedor, String> {
 }
